9c2fcb5b9d61e133b6e33eaf358df86e03b78fad
Anushka <anushkasehrawat98@gmail.com>
2019-10-07 15:17:32 +0530

    Have used tabs

--- WebSocketTest/Decoders/JsonDecoder.cs
+++ WebSocketTest/Decoders/JsonDecoder.cs
@@ -1,17 +1,15 @@
 ﻿using WebSocketTest.Datatypes;
 using System.Text.Json;
 
-
-
 namespace WebSocketTest.Decoders
 {
-	static class JsonDecoder
-	{
-		static public Inputs ParseToInputs(Inputs target)
-		{
-                string json = JsonSerializer.Serialize(target);
-                // Converts and returns json string to Inputs object
-                return JsonSerializer.Deserialize<Inputs>(json); 
-		}
-	}
+    static class JsonDecoder
+    {
+        static public Inputs ParseToInputs(Inputs target)
+        {
+            // Converts and returns json string to Inputs object
+            string json = JsonSerializer.Serialize(target);
+            return JsonSerializer.Deserialize<Inputs>(json);
+        }
+    }
 }
