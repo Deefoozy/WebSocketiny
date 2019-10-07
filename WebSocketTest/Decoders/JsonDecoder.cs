cee866376706c2f2b63c64edc6bb5140c3304431
tjeerddie <tjeerdverschragen@hotmail.com>
2019-10-07 19:10:08 +0200

    Add general json to dict deserialization

--- WebSocketTest/Decoders/JsonDecoder.cs
+++ WebSocketTest/Decoders/JsonDecoder.cs
@@ -1,4 +1,7 @@
-﻿using WebSocketTest.Datatypes;
+﻿using System.Collections;
+using WebSocketTest.Datatypes;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace WebSocketTest.Decoders
 {
@@ -8,5 +11,11 @@ namespace WebSocketTest.Decoders
 		{
 			return new Inputs();
 		}
+
+        static public IDictionary JsonToDictionary(string json)
+        {
+            var options = new JsonSerializerOptions { AllowTrailingCommas = true };
+            return JsonSerializer.Deserialize<Dictionary<string, string>>(json, options);
+        }
 	}
 }
