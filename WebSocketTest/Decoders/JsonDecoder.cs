474a47d4c12376968bf82eb2f155cec694dff102
tjeerddie <tjeerdverschragen@hotmail.com>
2019-10-07 19:19:16 +0200

    Sort imports and change spaces to tabs

--- WebSocketTest/Decoders/JsonDecoder.cs
+++ WebSocketTest/Decoders/JsonDecoder.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using WebSocketTest.Datatypes;
+﻿using WebSocketTest.Datatypes;
 using System.Text.Json;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace WebSocketTest.Decoders
@@ -12,10 +12,10 @@ namespace WebSocketTest.Decoders
 			return new Inputs();
 		}
 
-        static public IDictionary JsonToDictionary(string json)
-        {
-            var options = new JsonSerializerOptions { AllowTrailingCommas = true };
-            return JsonSerializer.Deserialize<Dictionary<string, string>>(json, options);
-        }
+		static public IDictionary JsonToDictionary(string json)
+		{
+			var options = new JsonSerializerOptions { AllowTrailingCommas = true };
+			return JsonSerializer.Deserialize<Dictionary<string, string>>(json, options);
+		}
 	}
 }
