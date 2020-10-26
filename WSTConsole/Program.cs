7846e3db80151939b8ee6122a141c461dd5f9187
Deefoozy <tinotrok@outlook.com>
2020-10-26 12:21:33 +0100

    Updated console code without threading

--- WSTConsole/Program.cs
+++ WSTConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using WebSocketiny;
+using WebSocketiny.Datatypes;
 
 namespace WSTConsole
 {
@@ -6,7 +9,40 @@ namespace WSTConsole
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var tcpConfig = new TcpConfig
+			{
+				ipAddress = IPAddress.Any,
+				port = 6942,
+			};
+
+			var tcpServer = new TcpServer(tcpConfig);
+
+			tcpServer.ReceivedMessage += ReceivedMessageHandler;
+
+			tcpServer.Init();
+		}
+
+		private static void ReceivedMessageHandler(string message, int user) {
+			Console.WriteLine($"Message:");
+			Console.WriteLine($"id {user} | {message}");
+		}
+
+		private static void ConnectHandler(Client connectedClient)
+		{
+			Console.WriteLine($"Connect:");
+			Console.WriteLine($"id {connectedClient.id}");
+		}
+
+		private static void DisconnectHandler(Client connectedClient)
+		{
+			Console.WriteLine($"Disconnect:");
+			Console.WriteLine($"id {connectedClient.id}");
+		}
+
+		private static void ErrorHandler(Exception exception)
+		{
+			Console.WriteLine($"Error:");
+			Console.WriteLine($"Exception {exception}");
 		}
 	}
 }
